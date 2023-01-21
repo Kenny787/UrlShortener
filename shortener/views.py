afed472adf37bcb0bea829587e7f9853dac67ccd
Kenny787 <kennyan.code@gmail.com>
2023-01-22 00:42:55 +0700

    Add base.html

--- shortener/views.py
+++ shortener/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'test.html')
+    return render(request, 'base.html')
 
 
 def createShortURL(request):
-    return render(request, 'test.html')
+    return render(request, 'base.html')
 
 
 def redirect(request):
-    return render(request, 'test.html')
+    return render(request, 'base.html')
 
 
 # Create your views here.
